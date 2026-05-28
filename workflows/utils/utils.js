a2c3847450a9fd4607fe001261c78b6fca3748e8
孙一 <sunyiex@163.com>
2026-05-28 10:07:50 +0800

    Update utils.js

--- workflows/utils/utils.js
+++ workflows/utils/utils.js
@@ -5,12 +5,22 @@ module.exports = {
   randomRangeNumber(start = 500, end = 1000) {
     return (Math.random() * (end - start) + start) >> 0;
   },
+  /**
+   * 清理cookie中的无效字符（如换行符、回车符、制表符等）
+   * @param {string} cookie
+   * @returns {string}
+   */
+  sanitizeCookie(cookie) {
+    if (!cookie) return cookie;
+    // 移除换行符、回车符、制表符等无效字符
+    return cookie.replace(/[\r\n\t]/g, '').trim();
+  },
   getUsersCookie(env) {
     const users = [env.COOKIE];
 
     const keys = Object.keys(env).filter(key => key.match(/^COOKIE_([0-9])+$/));
     keys.forEach(key => users.push(env[key]));
 
-    return users.filter(cookie => !!cookie);
+    return users.filter(cookie => !!cookie).map(cookie => this.sanitizeCookie(cookie));
   }
 };
